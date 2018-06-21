f0041bb6d49c601851387b0dacf4a908efb131e5
Deep <nfproperty2069>
2018-06-21 10:48:33 +0530

    no of hit

--- models/hits.js
+++ models/hits.js
@@ -2,20 +2,28 @@ var mongoose = require('mongoose');
 
 // Hit Schema
 var HitSchema = mongoose.Schema({
-	hits: {
+	name: {
+		type: String
+	},
+	count: {
 		type: Number
-	}
+	},
 });
 
 var Hit = module.exports = mongoose.model('Hit', HitSchema);
 
-module.exports.gethits = function(callback){
-	var query = {};
-	User.findOne(query, callback);
+module.exports.createHit = function(newHit, callback){
+
+	        newHit.save(callback);
 }
 
-module.exports.saveHit = function(callback){
-  Hit.find({}, function(hits){
-  console.log(hits);
-  })
+module.exports.gethits = function(callback){
+	var query = {"name" : 'hits'};
+	Hit.findOne(query, callback);
+}
+
+module.exports.updateHit = function(callback){
+		console.log('i am in hittt')
+		Hit.update({ name : "hits"},{$inc : {count : 1}},callback)
+
 }
--- routes/users.js
+++ routes/users.js
@@ -4,75 +4,126 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
 var User = require('../models/user');
+var Hit = require('../models/hits');
 
 // Register
-router.get('/register', function(req, res){
-	res.render('register');
+router.get('/register', function(req, res) {
+  res.render('register');
+});
+
+var newHit = new Hit({
+	name: 'hits',
+	count: 0
 });
 
 // Login
-router.get('/login', function(req, res){
-	res.render('login');
+router.get('/login', function(req, res) {
+
+	Hit.updateHit(function(err, hit) {
+
+		if(err) throw err;
+		console.log(hit);
+
+		Hit.gethits(function(err, hit) {
+
+			if(err) throw err;
+			  console.log(hit.count);
+				res.render('login',{name : hit.count});
+
+		});
+
+	});
+
+
+
+
+
+	// Hit.createHit(newHit, function(err, user) {
+  //     if (err) throw err;
+  //     console.log(user);
+  //   });
+
+
+
+	// Hit.updateHit(function(err, hit) {
+	//
+	// 	if(err) throw err;
+	// 	console.log(hit);
+	//
+	// });
+
+	// Hit.gethits(function(err, hit) {
+	//
+	// 	if(err) throw err;
+	// 	console.log(hit.count);
+	//
+	// });
+
+
 });
 
 // Register User
-router.post('/register', function(req, res){
-	var name = req.body.name;
-	var email = req.body.email;
-	var username = req.body.username;
-	var password = req.body.password;
-	var password2 = req.body.password2;
+router.post('/register', function(req, res) {
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
 
-	// Validation
-	req.checkBody('name', 'Name is required').notEmpty();
-	req.checkBody('email', 'Email is required').notEmpty();
-	req.checkBody('email', 'Email is not valid').isEmail();
-	req.checkBody('username', 'Username is required').notEmpty();
-	req.checkBody('password', 'Password is required').notEmpty();
-	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+  // Validation
+  req.checkBody('name', 'Name is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+  req.checkBody('username', 'Username is required').notEmpty();
+  req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
 
-	var errors = req.validationErrors();
+  var errors = req.validationErrors();
 
-	if(errors){
-		res.render('register',{
-			errors:errors
-		});
-	} else {
-		var newUser = new User({
-			name: name,
-			email:email,
-			username: username,
-			password: password
-		});
+  if (errors) {
+    res.render('register', {
+      errors: errors
+    });
+  } else {
+    var newUser = new User({
+      name: name,
+      email: email,
+      username: username,
+      password: password
+    });
 
-		User.createUser(newUser, function(err, user){
-			if(err) throw err;
+		User.createUser(newUser, function(err, user) {
+			if (err) throw err;
 			console.log(user);
 		});
 
-		req.flash('success_msg', 'You are registered and can now login');
+    req.flash('success_msg', 'You are registered and can now login');
 
-		res.redirect('/users/login');
-	}
+    res.redirect('/users/login');
+  }
 });
 
 passport.use(new LocalStrategy(
   function(username, password, done) {
-   User.getUserByUsername(username, function(err, user){
-   	if(err) throw err;
-   	if(!user){
-   		return done(null, false, {message: 'Unknown User'});
-   	}
+    User.getUserByUsername(username, function(err, user) {
+      if (err) throw err;
+      if (!user) {
+        return done(null, false, {
+          message: 'Unknown User'
+        });
+      }
 
-   	User.comparePassword(password, user.password, function(err, isMatch){
-   		if(err) throw err;
-   		if(isMatch){
-   			return done(null, user);
-   		} else {
-   			return done(null, false, {message: 'Invalid password'});
-   		}
-   	});
-   });
+      User.comparePassword(password, user.password, function(err, isMatch) {
+        if (err) throw err;
+        if (isMatch) {
+          return done(null, user);
+        } else {
+          return done(null, false, {
+            message: 'Invalid password'
+          });
+        }
+      });
+    });
   }));
 
 passport.serializeUser(function(user, done) {
@@ -86,17 +137,21 @@ passport.deserializeUser(function(id, done) {
 });
 
 router.post('/login',
-  passport.authenticate('local', {successRedirect:'/', failureRedirect:'/users/login',failureFlash: true}),
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login',
+    failureFlash: true
+  }),
   function(req, res) {
     res.redirect('/');
   });
 
-router.get('/logout', function(req, res){
-	req.logout();
+router.get('/logout', function(req, res) {
+  req.logout();
 
-	req.flash('success_msg', 'You are logged out');
+  req.flash('success_msg', 'You are logged out');
 
-	res.redirect('/users/login');
+  res.redirect('/users/login');
 });
 
 module.exports = router;
